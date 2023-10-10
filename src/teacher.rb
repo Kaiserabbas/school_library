8c8fed25d8440b0c47ec97df65073e6396f9d838
Qaisar <kayser.abbas@gmail.com>
2023-10-10 08:34:29 +0500

    Correct the linters errors

--- src/teacher.rb
+++ src/teacher.rb
@@ -1,9 +1,10 @@
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age, name = "Unknown", parent_permission = true, specialization = "Unknown")
-    super(id, age, name, parent_permission)
+  def initialize(id, age, name: 'Unknown', parent_permission: true, specialization: 'Unknown')
+    super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
+    @courses_taught = []
   end
 
   def can_use_services?
