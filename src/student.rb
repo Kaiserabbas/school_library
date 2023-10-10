811c914d2346853a7714a5ccc20048bb814bf01a
Qaisar <kayser.abbas@gmail.com>
2023-10-10 08:34:26 +0500

    Correct the linters errors

--- src/student.rb
+++ src/student.rb
@@ -3,12 +3,13 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, age, name = "Unknown", parent_permission = true, classroom = "Unknown")
-    super(id, age, name, parent_permission)
+  def initialize(id, age, name: 'Unknown', parent_permission: true, classroom: 'Unknown')
+    super(id, age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    @courses = []
   end
 
   def play_hooky
-    "¯\\(ツ)/¯"
+    '¯\\(ツ)/¯'
   end
 end
