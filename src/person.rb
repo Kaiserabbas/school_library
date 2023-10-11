a2d2a16639e1e0bc86bdb75e641f63b5690d94f3
Qaisar <kayser.abbas@gmail.com>
2023-10-11 07:49:58 +0500

    Provide the age parameter first, followed by the name parameter.

--- src/person.rb
+++ src/person.rb
@@ -33,7 +33,7 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
+person = Person.new(22, name: 'Maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
