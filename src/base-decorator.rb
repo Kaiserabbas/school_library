d859d5ed69c523807f3389015c34118129c895e3
Qaisar <kayser.abbas@gmail.com>
2023-10-11 07:32:09 +0500

    Create a base decorator class that inherits from Nameable

---
+++ src/base-decorator.rb
@@ -0,0 +1,13 @@
+require 'Nameable'
+
+class BaseDecorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
