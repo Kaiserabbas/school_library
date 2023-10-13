420b341e4a2ecafe3dfa1cc5e2a2b9da45cc5889
Qaisar <kayser.abbas@gmail.com>
2023-10-13 09:33:19 +0500

    Update the code to match project requirements

--- src/main.rb
+++ src/main.rb
@@ -1,71 +1,40 @@
-require_relative 'app'  # Assuming that your application code is in 'app.rb'
+require_relative 'app'
 
-def main
-  puts 'Welcome to the School Library Management System'
-  loop do
-    puts "\nOptions:"
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List rentals for a person'
-    puts '7. Quit'
-
-    print 'Please select an option: '
-    choice = gets.chomp.to_i
-
-    case choice
-    when 1
-      list_books(books)
-    when 2
-      list_people(people)
-    when 3
-      print 'Enter the person type (student/teacher): '
-      person_type = gets.chomp
-      print 'Enter the person ID: '
-      person_id = gets.chomp.to_i
-      print 'Enter the person age: '
-      person_age = gets.chomp.to_i
-      print 'Enter the person name: '
-      person_name = gets.chomp
-      if person_type == 'student'
-        print 'Enter the classroom: '
-        classroom = gets.chomp
-        create_person(person_type, person_id, person_age, person_name, classroom: classroom)
-      elsif person_type == 'teacher'
-        print 'Enter the specialization: '
-        specialization = gets.chomp
-        create_person(person_type, person_id, person_age, person_name, specialization: specialization)
-      else
-        puts 'Invalid person type.'
-      end
-    when 4
-      print 'Enter the book title: '
-      book_title = gets.chomp
-      print 'Enter the book author: '
-      book_author = gets.chomp
-      create_book(book_title, book_author)
-    when 5
-      print 'Enter the rental date: '
-      rental_date = gets.chomp
-      print 'Enter the book title: '
-      book_title = gets.chomp
-      print 'Enter the person ID: '
-      person_id = gets.chomp.to_i
-      create_rental(rental_date, book_title, person_id, books, people)
-    when 6
-      print 'Enter the person ID to list rentals: '
-      person_id = gets.chomp.to_i
-      list_rentals(person_id, rentals)
-    when 7
-      puts 'Exiting the app. Goodbye!'
-      break
-    else
-      puts 'Invalid option. Please choose a valid option.'
-    end
+puts "Welcome to School Library App\n\n"
+MENU = "Please choose an option by entering a desired number:\n\
+1 - List all books\n\
+2 - List all people \n\
+3 - Create a person\n\
+4 - Create a book\n\
+5 - Create a rental\n\
+6 - List all rentals for a given person ID\n\
+7 - Exit".freeze
+APP = App.new
+def switch(get)
+  case get
+  when 1
+    APP.list_books
+  when 2
+    APP.list_people
+  when 3
+    APP.create_person
+  when 4
+    APP.create_book
+  when 5
+    APP.create_rental
+  when 6
+    APP.list_rentals
   end
 end
 
-# Run the main method when this file is executed
+def main
+  get = 0
+  while get != 7 and get < 7
+    puts MENU
+    get = gets.chomp.to_i
+    switch(get)
+  end
+  puts 'Thank you for using this app!'
+end
+
 main
