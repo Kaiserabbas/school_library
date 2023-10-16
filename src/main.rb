674d28e606c40a48030b3c82bafefa70ba041a8e
Bett Enock <bettenock968@gmail.com>
2023-10-16 16:09:53 -0700

    Refactor code to meet SOLID principles.

--- src/main.rb
+++ src/main.rb
@@ -1,40 +1,48 @@
 require_relative 'app'
 
-puts "Welcome to School Library App\n\n"
-MENU = "Please choose an option by entering a desired number:\n\
-1 - List all books\n\
-2 - List all people \n\
-3 - Create a person\n\
-4 - Create a book\n\
-5 - Create a rental\n\
-6 - List all rentals for a given person ID\n\
-7 - Exit".freeze
-APP = App.new
-def switch(get)
-  case get
-  when 1
-    APP.list_books
-  when 2
-    APP.list_people
-  when 3
-    APP.create_person
-  when 4
-    APP.create_book
-  when 5
-    APP.create_rental
-  when 6
-    APP.list_rentals_for_person
+class Main
+  MENU = "Please choose an option by entering a desired number:\n\
+  1 - List all books\n\
+  2 - List all people \n\
+  3 - Create a person\n\
+  4 - Create a book\n\
+  5 - Create a rental\n\
+  6 - List all rentals for a given person ID\n\
+  7 - Exit".freeze
+
+  def initialize
+    @app = App.new
+  end
+
+  def run
+    puts "Welcome to School Library App\n\n"
+    get = 0
+    while get != 7
+      puts MENU
+      get = gets.chomp.to_i
+      handle_option(get)
+    end
+    puts 'Thank you for using this app!'
+  end
+
+  private
+
+  def handle_option(option)
+    case option
+    when 1
+      @app.list_books
+    when 2
+      @app.list_people
+    when 3
+      @app.create_person
+    when 4
+      @app.create_book
+    when 5
+      @app.create_rental
+    when 6
+      @app.list_rentals_for_person
+    end
   end
 end
 
-def main
-  get = 0
-  while get != 7 and get < 7
-    puts MENU
-    get = gets.chomp.to_i
-    switch(get)
-  end
-  puts 'Thank you for using this app!'
-end
-
-main
+Main.new.run
